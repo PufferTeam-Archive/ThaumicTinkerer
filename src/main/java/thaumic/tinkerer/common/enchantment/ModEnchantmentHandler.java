7003a73add51a984df8ca737943483c94d2e3a2b
RubilaxXxx <162358670+RubilaxXxx@users.noreply.github.com>
2024-09-24 16:14:00 +0200

    changed dispersed/focused strike cause it was absurd.

--- src/main/java/thaumic/tinkerer/common/enchantment/ModEnchantmentHandler.java
+++ src/main/java/thaumic/tinkerer/common/enchantment/ModEnchantmentHandler.java
@@ -102,17 +102,16 @@ public class ModEnchantmentHandler {
                     heldItem.stackTagCompound.setLong(NBTLastTarget, entityId.getMostSignificantBits());
                     heldItem.stackTagCompound.setLong(NBTLastTarget2, entityId.getLeastSignificantBits());
                 } else {
-                    successiveStrikes = Math.max(successiveStrikes + 1, 5);
+                    if(successiveStrikes < 5)
+                        successiveStrikes++;
                 }
                 heldItem.stackTagCompound.setInteger(NBTSuccessiveStrike, successiveStrikes);
 
                 if (focusedStrikes > 0) {
-                    event.ammount /= 2;
-                    event.ammount += (.5 * Math.max(successiveStrikes, 5) * event.ammount * focusedStrikes);
+                    event.ammount /=  2 * successiveStrikes * focusedStrikes;
                 }
                 if (dispersedStrikes > 0) {
-                    event.ammount *= 1 + 0.6 * dispersedStrikes + (successiveStrikes / 5);
-                    event.ammount /= 1 + 0.25 * dispersedStrikes + (successiveStrikes * 2.5);
+                    event.ammount *= 2 / successiveStrikes * dispersedStrikes;
                 }
             }
 
